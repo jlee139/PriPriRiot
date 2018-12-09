ba89731713b8a0314fdc6c095cebfee4969130ca
jlee <jlee139@gmu.edu>
2018-12-09 13:37:14 -0500

    Set Up
    More setting up lalalala

--- Assets/Script/BaseScript.cs
+++ Assets/Script/BaseScript.cs
@@ -1,16 +1,43 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class BaseScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    DialogueParser Parser;
+    int curline, curDay;
+    string curExpoMes, curHeader, curSubT;
+
+    [SerializeField] Canvas messageCanvas;
+    [SerializeField] Text messageText;
+    [SerializeField] Text subText;
+    [SerializeField] Text HeadText;
+
+    // Use this for initialization
+    void Start () {
+        //Get our dialogue
+        Parser = GameObject.Find("DialogueParser").GetComponent<DialogueParser>();
+        curline = 0;
+        curDay = 1;
+
+        //Game preparing
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButtonDown(0) )
+        {
+            ShowDialogue();
+            curline++;
+        }
+    }
+
+    public void ShowDialogue()
+    {
+       
+    }
+    
+
+
 }
---
+++ Assets/Script/DialogueParser.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+public class DialogueParser : MonoBehaviour
+{
+
+    struct EachLine
+    {
+        public string content;
+        public string location;
+        public int newpage;
+    }
+
+    List<EachLine> alllines;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        alllines = new List<EachLine>();
+
+        for (int n=1;n<=7;n++) {
+            //Text preparing
+            string file = "Assets/Text/Day" + n + ".txt";
+            LoadDialogue(file);
+        }
+        
+    }
+
+
+    void LoadDialogue(string filename)
+    {
+        string line;
+        StreamReader r = new StreamReader(filename);
+
+        using (r)
+        {
+            do
+            {
+                line = r.ReadLine();
+                if (line != null)
+                {
+                    string[] lineData = line.Split(';');
+                    EachLine lineEntry = new EachLine();
+                    lineEntry.content = lineData[0];
+                    lineEntry.location = lineData[1];
+                    if (lineData[2] == "0") lineEntry.newpage = 0;
+                    else lineEntry.newpage = 1;
+                    alllines.Add(lineEntry);
+                }
+            } while (line != null);
+            r.Close();
+        }
+    }
+
+    public string GetLocation(int linenumber)
+    {
+        if (linenumber < alllines.Count)
+        {
+            return alllines[linenumber].location;
+        }
+        return "";
+    }
+
+    public int GetNewPage(int linenumber)
+    {
+        if (linenumber < alllines.Count)
+        {
+            return alllines[linenumber].newpage;
+        }
+        return 0;
+    }
+
+    public string GetContent(int linenumber)
+    {
+        if (linenumber < alllines.Count)
+        {
+            return alllines[linenumber].content;
+        }
+        return "";
+    }
+
+
+   
+}
