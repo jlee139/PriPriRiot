57e22b917576cebc91f66a43c43fb69b9ef5be0e
jlee <jlee139@gmu.edu>
2018-12-09 20:31:10 -0500

    Create 15 Random Criminals
    Criminals are now created randomly and their difficulties set. Need to set up their dialogue options.

--- Assets/Script/BaseScript.cs
+++ Assets/Script/BaseScript.cs
@@ -16,8 +16,19 @@ public class BaseScript : MonoBehaviour {
         public int newpage;
     }
 
+    struct Prisoner
+    {
+        public string name;
+        public int difficulty;
+        public bool converted;
+        public string crime;
+    }
+
     List<EachLine> alllines;
-    
+    List<Prisoner> allprisoners;
+    List<string> Allnames;
+    List<string> Allcrimes;
+
     int curline, curDay;
     bool vnmode;
 
@@ -27,7 +38,7 @@ public class BaseScript : MonoBehaviour {
     [SerializeField] Text HeadText;
 
     // Use this for initialization
-    void Start () {
+    void Start() {
         //We start in vn mode
         vnmode = true;
 
@@ -45,12 +56,18 @@ public class BaseScript : MonoBehaviour {
         curDay = 1;
 
         //Game preparing
+        allprisoners = new List<Prisoner>();
+        Allnames = new List<string>();
+        Allcrimes = new List<string>();
 
+        PopulateName();
+        PopulateCrime();
+        CreatePrisoners();
 
     }
-    
+
     // Update is called once per frame
-    void Update () {
+    void Update() {
         //If we're in vn mode and the player presses down
         if (Input.GetMouseButtonDown(0) && vnmode)
         {
@@ -64,7 +81,7 @@ public class BaseScript : MonoBehaviour {
         //If the "new page" is 1, then we need to clear the information that's alerady been in there
         if (alllines[curline].newpage == 1)
         {
-            messageText.text="";
+            messageText.text = "";
             subText.text = "";
             HeadText.text = "";
 
@@ -95,7 +112,7 @@ public class BaseScript : MonoBehaviour {
             if (alllines[curline].location.Equals("T"))
             {
                 string tempholder = messageText.text;
-               messageText.text = tempholder+ "\n\n" + alllines[curline].content;
+                messageText.text = tempholder + "\n\n" + alllines[curline].content;
             }
         }
     }
@@ -104,8 +121,10 @@ public class BaseScript : MonoBehaviour {
     {
         //Use messageText.text to show the current state of the prison
 
+
     }
 
+    //Load Data from file just once
     void LoadDialogue(string filename)
     {
         string line;
@@ -131,8 +150,78 @@ public class BaseScript : MonoBehaviour {
         }
     }
 
+    //Create 15 prisoners to interact with
+    void CreatePrisoners()
+    {
+        for(int i=0; i < 15; i++)
+        {
+            Prisoner temp = new Prisoner();
 
+            //Set a random name
+            int random = Random.Range(0, Allnames.Count);
+            temp.name = Allnames[random];
+            //delete name from Allnames so we don't get a repeat
+            Allnames.RemoveAt(random);
 
+            //Set a random difficulty for this guy
+            temp.difficulty = Random.Range(0,5);
 
+            //Set a random crime
+            random = Random.Range(0, Allcrimes.Count);
+            temp.crime = Allcrimes[random];
+
+            //Set converted to false unless difficulty is 0
+            if (temp.difficulty == 0) temp.converted = true;
+            else temp.converted = false;
+
+            Debug.Log("Name: "+temp.name+"\tCrime: "+temp.crime+"\tDifficulty: "+temp.difficulty);
+
+            allprisoners.Add(temp);
+        }
+     }
+
+    //Read from the file "Name.text" 
+    void PopulateName()
+    {
+        string line;
+        StreamReader r = new StreamReader("Assets/Text/Name.txt");
+
+        using (r)
+        {
+            do
+            {
+                line = r.ReadLine();
+                if (line != null)
+                {
+                    Allnames.Add(line);
+                }
+            } while (line != null);
+            r.Close();
+        }
+
+    }
+
+    //Read from file "Crime.text"
+    void PopulateCrime()
+    {
+         string line;
+        StreamReader r = new StreamReader("Assets/Text/Crime.txt");
+
+        using (r)
+        {
+            do
+            {
+                line = r.ReadLine();
+                if (line != null)
+                {
+                    //string[] lineData = line.Split(';');
+                    //Allcrimes.Add(lineData[0]);
+                    Allcrimes.Add(line);
+                }
+            } while (line != null);
+            r.Close();
+        }
+
+    }
 
 }
